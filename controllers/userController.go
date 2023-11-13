33da62546082627b642d6551e1ce299f68c8a9b3
Harsh Singh <harsh@Harshs-MacBook-Air.local>
2023-11-14 01:36:12 +0530

    feat: add PUT users API

--- controllers/userController.go
+++ controllers/userController.go
@@ -82,3 +82,50 @@ func NewUser() gin.HandlerFunc {
 		c.JSON(http.StatusOK, gin.H{"id": result.InsertedID})
 	}
 }
+
+func UpdateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+		firstName := c.Request.Header.Get("first_name")
+		email := c.Request.Header.Get("email")
+
+		if firstName == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "first name must not be empty"})
+			return
+		}
+
+		if email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "email must not be empty"})
+			return
+		}
+
+		objId, err := primitive.ObjectIDFromHex(userId)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"invalid input": err.Error()})
+			return
+		}
+
+		// database
+		if err = database.StartMongoDB(); err != nil {
+			log.Fatal("Unable to Start a New MongoDB server")
+		}
+		collection := database.GetCollection("users")
+		defer database.CloseMongoDB()
+
+		filter := bson.M{"_id": objId}
+		update := bson.M{
+			"$set": bson.M{
+				"first_name": firstName,
+				"email":      email,
+			},
+		}
+
+		result, err := collection.UpdateOne(context.Background(), filter, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"updated_count": result.ModifiedCount})
+	}
+}
